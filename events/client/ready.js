8ba34d50ab2ccabe9e758a54367c9eddd1efc2a6
naksudev <xersinn@gmail.com>
2023-04-02 18:48:30 +0200

    sdfjmklqsldmkfjcwxv

--- events/client/ready.js
+++ events/client/ready.js
@@ -10,5 +10,27 @@ module.exports = {
         console.log(chalk.green(`[✔] Logged in as ${client.user.tag}`));
 
         client.application.commands.set(client.commands.map(cmd => cmd));
+
+        const CronJob = require('cron').CronJob;
+
+        // Créer une nouvelle tâche planifiée pour s'exécuter tous les jours à 23 heures
+        const job = new CronJob('0 0 23 * * *', function() {
+            console.log('Log envoyé à 23h tous les jours !');
+            client.channels.fetch('1089243752448659457')
+            .then(channel => {
+                channel.send({content : 'Bonne nuit <@308327218197561344>!', files: [{ attachment: '/zenaii/assets/img/kavish/naruto.png', name: 'homie.png' }]});
+            })
+        });
+
+        const job = new CronJob('0 50 18 * * *', function() {
+            console.log('Log envoyé à 23h tous les jours !');
+            client.channels.fetch('1089243752448659457')
+            .then(channel => {
+                channel.send({content : 'Bonne nuit <@308327218197561344>!', files: [{ attachment: '/zenaii/assets/img/kavish/naruto.png', name: 'homie.png' }]});
+            })
+        });
+
+        // Démarre la tâche planifiée
+        job.start();
     }
 };
